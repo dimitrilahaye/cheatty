489f4699c306f501b4a3b6d4c62952b0df9ab288
Dimitri Lahaye <contact@dimitrilahaye.net>
2021-05-02 00:07:35 +0200

    fix(ansible): fix ansible and env for api (#6)
    * fix(ansible): fix ansible and env for api

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,24 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
-import { environment } from './environment';
 import { AppResolver } from './app.resolver';
-
-const root =
-  process.env.PRODUCTION || environment.server.production
-    ? 'mongodb+srv'
-    : 'mongodb';
-const USR = process.env.DB_USER_NAME || environment.db.username;
-const PWD = process.env.DB_PASSWORD || environment.db.password;
-const HOST = process.env.DB_HOST || environment.db.host;
-const DATABASE = process.env.DB_NAME || environment.db.database;
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: `.env.${process.env.NODE_ENV}` }),
     TypeOrmModule.forRoot({
-      type: environment.db.type,
-      url: `${root}://${USR}:${PWD}@${HOST}/${DATABASE}?retryWrites=true&w=majority`,
+      type: 'mongodb',
+      url: `${
+        process.env.PRODUCTION === 'true' ? 'mongodb+srv' : 'mongodb'
+      }://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}/${
+        process.env.DATABASE
+      }?retryWrites=true&w=majority`,
       synchronize: true,
       useUnifiedTopology: true,
       entities: [],
