b085fbcdd6c5390f65ebfec17e543de996f93b96
Dimitri Lahaye <contact@dimitrilahaye.net>
2021-05-01 23:58:03 +0200

    fix(ansible): fix ansible and env for api

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,14 +1,14 @@
+require('dotenv').config({ path: `../.env.${process.env.NODE_ENV}` });
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from '@nestjs/common';
-import { environment } from './environment';
 import * as Sentry from '@sentry/node';
 import { SentryInterceptor } from './interceptors/sentry.interceptor';
 
 async function bootstrap() {
-  const SENTRY = process.env.SENTRY || environment.sentry.url;
-  const ORIGIN = process.env.ORIGIN || environment.server.origin;
+  const SENTRY = process.env.SENTRY;
+  const ORIGIN = process.env.ORIGIN;
   const logger = new Logger('bootstrap');
 
   const app = await NestFactory.create(AppModule);
@@ -23,11 +23,11 @@ async function bootstrap() {
 
   Sentry.init({
     dsn: SENTRY,
-    tracesSampleRate: environment.sentry.traces,
+    tracesSampleRate: Number(process.env.TRACES),
   });
   logger.log(`Sentry initialized`);
 
-  const port = process.env.PORT || environment.server.port;
+  const port = process.env.PORT;
   await app.listen(port);
   logger.log(`Server is listening on port ${port}`);
 }
