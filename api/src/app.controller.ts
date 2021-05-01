561eff9909509c47882601962b8fee6e5696128f
Dimitri Lahaye <contact@dimitrilahaye.net>
2021-05-02 00:02:45 +0200

    fix lint

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,9 @@
-import { Controller, Get, InternalServerErrorException } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get()
   healthCheck(): any {
-    // test sentry
-    // throw new InternalServerErrorException();
-
     return {
       uptime: process.uptime(),
       message: 'OK',
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 require('dotenv').config({ path: `../.env.${process.env.NODE_ENV}` });
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
