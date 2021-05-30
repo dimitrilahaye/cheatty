9fdf27341c860abe361b0ba64fa1eb9003db5c64
Dimitri Lahaye <contact@dimitrilahaye.net>
2021-05-30 21:55:24 +0200

    core(Terraform) add scaleway_instance_security_group

--- terraform/scaleway.tf
+++ terraform/scaleway.tf
@@ -17,12 +17,34 @@ provider "scaleway" {
 
 resource "scaleway_instance_ip" "public_ip" {}
 
+resource "scaleway_instance_security_group" "cheatty-instance" {
+  inbound_default_policy  = "drop"
+  outbound_default_policy = "accept"
+
+  inbound_rule {
+    action = "accept"
+    port   = "22" # ssh, scp, sftp ports
+  }
+
+  inbound_rule {
+    action = "accept"
+    port   = "80" # http default port
+  }
+
+  inbound_rule {
+    action = "accept"
+    port   = "443" # https secure transfers
+  }
+}
+
 resource "scaleway_instance_server" "cheatty-instance" {
   type  = "DEV1-S"
   image = "debian_buster"
   name = "cheatty-staging"
   tags = [ "terraform instance", "cheatty-staging" ]
   ip_id = var.ip_id
+
+  security_group_id = scaleway_instance_security_group.cheatty-instance.id
 }
 
 output "cheatty-staging" {
