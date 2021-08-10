4a3bb8029856f73ce8dbadd35b0830185c97972f
Dimitri Lahaye <contact@dimitrilahaye.net>
2021-08-11 01:41:39 +0200

    core(ansible) fix env vars and app service

--- app/src/lib/Env.ts
+++ app/src/lib/Env.ts
@@ -1,6 +1,6 @@
 export default {
-	NODE_ENV: import.meta.env.VITE_NODE_ENV,
-	API: import.meta.env.VITE_API,
-	SENTRYURL: import.meta.env.VITE_SENTRYURL,
-	SENTRYTRACES: import.meta.env.VITE_SENTRYTRACES
+	NODE_ENV: import.meta.env.VITE_NODE_ENV || process.env.NODE_ENV,
+	API: import.meta.env.VITE_API || process.env.API,
+	SENTRYURL: import.meta.env.VITE_SENTRYURL || process.env.SENTRYURL,
+	SENTRYTRACES: import.meta.env.VITE_SENTRYTRACES || process.env.SENTRYTRACES
 };
