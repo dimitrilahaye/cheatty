6e91a66e481002920a305c95475c7e635cfbb287
Dimitri Lahaye <contact@dimitrilahaye.net>
2021-08-11 00:30:50 +0200

    core(ts) fix ci build

--- app/src/style/TailwindCSS.svelte
+++ app/src/style/TailwindCSS.svelte
@@ -1,11 +1,11 @@
 <style global lang="postcss">
-  @import 'tailwindcss/base';
-  @import 'tailwindcss/components';
-  @import 'tailwindcss/utilities';
+	@import 'tailwindcss/base';
+	@import 'tailwindcss/components';
+	@import 'tailwindcss/utilities';
 
-  html,
-  body {
-    margin: 0;
-    @apply bg-blue-100;
-  }
+	html,
+	body {
+		margin: 0;
+		@apply bg-blue-100;
+	}
 </style>
