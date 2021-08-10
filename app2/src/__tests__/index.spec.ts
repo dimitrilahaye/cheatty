cd097a6db0f075554b375bb37c4dbc0569e95655
Dimitri Lahaye <contact@dimitrilahaye.net>
2021-08-10 23:28:06 +0200

    core(test) add jest to project

---
+++ app2/src/__tests__/index.spec.ts
@@ -0,0 +1,8 @@
+import Index from '../routes/index.svelte';
+import { render } from '@testing-library/svelte';
+
+test('Welcome the user', () => {
+	const { getByText } = render(Index);
+
+	expect(getByText('Welcome to SvelteKit')).toBeInTheDocument();
+});
