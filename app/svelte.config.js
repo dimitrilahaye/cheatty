312e3adb4055a31c610ee4e28f59f6093b3ad68c
Dimitri Lahaye <contact@dimitrilahaye.net>
2021-08-11 13:16:02 +0200

    core(ci) add env var for each environment

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,5 +1,4 @@
 import preprocess from 'svelte-preprocess';
-import autoPreprocess from 'svelte-preprocess'
 import node from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,21 +8,20 @@ const config = {
 	preprocess: [
 		preprocess({
         	"postcss": true
-    	}),
-		autoPreprocess({
-			replace: [
-				['process.env.NODE_ENV', JSON.stringify(process.env.NODE_ENV)],
-				['process.env.API', JSON.stringify(process.env.API)],
-				['process.env.SENTRYURL', JSON.stringify(process.env.SENTRYURL)],
-				['process.env.SENTRYTRACES', JSON.stringify(process.env.SENTRYTRACES)],
-			],
-		})
+    	})
 	],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		adapter: node(),
+		vite: {
+			server: {
+			  watch: {
+				usePolling: true,
+			  },
+			},
+		  },
 	}
 };
 
