d47439a2029b7df2e4df6432ce4622fa8c807f50
Dimitri Lahaye <contact@dimitrilahaye.net>
2021-08-11 02:37:19 +0200

    core(app vars env) preprocess env vars

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,13 +1,24 @@
 import preprocess from 'svelte-preprocess';
+import autoPreprocess from 'svelte-preprocess'
 import node from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess({
-        "postcss": true
-    })],
+	preprocess: [
+		preprocess({
+        	"postcss": true
+    	}),
+		autoPreprocess({
+			replace: [
+				['process.env.NODE_ENV', JSON.stringify(process.env.NODE_ENV)],
+				['process.env.API', JSON.stringify(process.env.API)],
+				['process.env.SENTRYURL', JSON.stringify(process.env.SENTRYURL)],
+				['process.env.SENTRYTRACES', JSON.stringify(process.env.SENTRYTRACES)],
+			],
+		})
+	],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
